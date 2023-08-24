dea467ff867be856da45c7edb60c137a4f64835a
Goulartx <vag.vinicius.goulart2006@gmail.com>
2023-08-24 11:42:26 -0300

    ART-10

--- form/cadastrar.php
+++ form/cadastrar.php
@@ -1,20 +1,17 @@
 <?php  
     include('conexao.php');
 
-        $nome = $_POST['nome_cad'];
-        $email = $_POST['email_cad'];
-        $senha = $_POST['senha_cad'];
-
+    $nome = $_POST['nome_cad'];
+    $email = $_POST['email_cad'];
+    $senha = $_POST['senha_cad'];
+    $nasc = $_POST['nasc_cad'];
+    $fone = $_POST['fone_cad'];
       
-        $mysqli_insert = "INSERT INTO `usuarios`(`us_nome`, `us_email`, `us_senha`) VALUES ('$nome', '$email', '$senha')";
+    $mysqli_insert = "INSERT INTO `usuarios`(`us_nome`, `us_email`, `us_senha`, `us_nasc`, `us_telefone`) VALUES ('$nome', '$email', '$senha', '$nasc', '$fone')";
 
-
-        if (mysqli_query($conexao, $mysqli_insert)) {
-            echo "Cadastro realizado com sucesso!";
-            
-        } else {
-            echo "Erro ao cadastrar: " . mysqli_error($conexao);
-            
-        }
-    
+    if (mysqli_query($conexao, $mysqli_insert)) {
+        echo "Cadastro realizado com sucesso!";
+    } else {
+        echo "Erro ao cadastrar: " . mysqli_error($conexao);
+    }
 ?>

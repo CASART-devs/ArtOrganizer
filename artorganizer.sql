2997ef278d94043bd88166ed0018ed0e70399b60
vinicius <vag.vinicius.goulart2006@gmail.com>
2024-08-16 22:06:48 -0300

    [ART 001] Comentado e formatado script `artorganizer.sql`

--- artorganizer.sql
+++ artorganizer.sql
@@ -1,104 +1,110 @@
-create or replace table artorganizer.pastas
-(
-    id         int auto_increment
-        primary key,
-    nome_pasta varchar(20) not null,
-    descricao  text        null
-)
-    collate = utf8mb4_general_ci;
+-- Criação da base de dados
+create or replace database artorganizer;
 
-create or replace table artorganizer.tags
-(
-    id       int auto_increment
-        primary key,
-    nome_tag varchar(100) not null
-)
-    collate = utf8mb4_general_ci;
+-- Seleção da base de dados para uso
+use artorganizer;
 
-create or replace table artorganizer.artigos
+-- Criação da tabela 'pastas'
+create or replace table pastas
 (
-    ID               int auto_increment
-        primary key,
-    Titulo           varchar(255)         null,
-    Autor            varchar(255)         null,
-    Data_Publicacao  datetime             null,
-    `img-previw`     varchar(100)         not null,
-    `artigo-caminho` varchar(100)         not null,
-    ID_Tag           int                  null,
-    privacidade      enum ('priv', 'pub') not null,
-    constraint artigos_ibfk_1
-        foreign key (ID_Tag) references artorganizer.tags (id)
+    id int auto_increment primary key,  -- Identificador único da pasta
+    nome_pasta varchar(20) not null,    -- Nome da pasta
+    descricao text null                 -- Descrição da pasta
 )
-    collate = utf8mb4_general_ci;
+    collate = utf8mb4_general_ci;       -- Collation para suportar caracteres especiais
 
-create or replace table artorganizer.artigo_pasta
+-- Criação da tabela 'tags'
+create or replace table tags
 (
-    id        int auto_increment
-        primary key,
-    id_pasta  int not null,
-    id_artigo int not null,
+    id int auto_increment primary key,  -- Identificador único da tag
+    nome_tag varchar(100) not null     -- Nome da tag
+)
+    collate = utf8mb4_general_ci;      -- Collation para suportar caracteres especiais
+
+-- Criação da tabela 'artigos'
+create or replace table artigos
+(
+    ID int auto_increment primary key,               -- Identificador único do artigo
+    Titulo varchar(255) null,                        -- Título do artigo
+    Autor varchar(255) null,                         -- Autor do artigo
+    Data_Publicacao datetime null,                   -- Data de publicação
+    `img-previw` varchar(100) not null,              -- Imagem de pré-visualização
+    `artigo-caminho` varchar(100) not null,          -- Caminho do artigo
+    ID_Tag int null,                                -- Referência à tag associada
+    privacidade enum ('priv', 'pub') not null,      -- Privacidade do artigo
+    constraint artigos_ibfk_1 foreign key (ID_Tag) references artorganizer.tags (id) -- Chave estrangeira para tags
+)
+    collate = utf8mb4_general_ci;                    -- Collation para suportar caracteres especiais
+
+-- Criação da tabela 'artigo_pasta'
+create or replace table artigo_pasta
+(
+    id int auto_increment primary key,              -- Identificador único da relação artigo-pasta
+    id_pasta int not null,                          -- Referência à pasta
+    id_artigo int not null,                         -- Referência ao artigo
     constraint artigo_pasta_ibfk_1
-        foreign key (id_pasta) references artorganizer.pastas (id),
+        foreign key (id_pasta) references artorganizer.pastas (id), -- Chave estrangeira para pastas
     constraint artigo_pasta_ibfk_2
-        foreign key (id_artigo) references artorganizer.artigos (ID),
-    constraint fk_artigos_artigo_pasta
-        foreign key (id_artigo) references artorganizer.artigos (ID)
-            on delete cascade
+        foreign key (id_artigo) references artorganizer.artigos (ID) -- Chave estrangeira para artigos
 )
-    collate = utf8mb4_general_ci;
+    collate = utf8mb4_general_ci;                    -- Collation para suportar caracteres especiais
 
+-- Criação do índice para a coluna 'id_pasta' na tabela 'artigo_pasta'
 create or replace index id_pasta
     on artorganizer.artigo_pasta (id_pasta);
 
+-- Criação do índice para a coluna 'ID_Tag' na tabela 'artigos'
 create or replace index ID_Tag
     on artorganizer.artigos (ID_Tag);
 
-create or replace table artorganizer.usuarios
+-- Criação da tabela 'usuarios'
+create or replace table usuarios
 (
-    ID            int auto_increment
-        primary key,
-    Nome_Usuario  varchar(255)   not null,
-    Senha         varchar(255)   not null,
-    Nome_Completo varchar(255)   null,
-    Email         varchar(255)   null,
-    Data_Nasc     date           not null,
-    telefone      int            null,
-    `img-perfil`  varchar(1000)  null,
-    permissoes    set ('1', '2') null
+    ID int auto_increment primary key,           -- Identificador único do usuário
+    Nome_Usuario varchar(255) not null,          -- Nome de usuário
+    Senha varchar(255) not null,                 -- Senha do usuário
+    Nome_Completo varchar(255) null,            -- Nome completo do usuário
+    Email varchar(255) null,                     -- Email do usuário
+    Data_Nasc date not null,                     -- Data de nascimento
+    telefone int null,                           -- Telefone do usuário
+    `img-perfil` varchar(1000) null,             -- Imagem de perfil
+    permissoes set ('1', '2') null               -- Permissões do usuário
 )
-    collate = utf8mb4_general_ci;
+    collate = utf8mb4_general_ci;                -- Collation para suportar caracteres especiais
 
+-- Criação da tabela 'pasta_user'
 create or replace table artorganizer.pasta_user
 (
-    id       int auto_increment
-        primary key,
-    id_user  int not null,
-    id_pasta int not null,
+    id int auto_increment primary key,              -- Identificador único da relação pasta-usuário
+    id_user int not null,                           -- Referência ao usuário
+    id_pasta int not null,                          -- Referência à pasta
     constraint pasta_user_ibfk_1
-        foreign key (id_user) references artorganizer.usuarios (ID),
+        foreign key (id_user) references artorganizer.usuarios (ID), -- Chave estrangeira para usuários
     constraint pasta_user_ibfk_2
-        foreign key (id_pasta) references artorganizer.pastas (id)
+        foreign key (id_pasta) references artorganizer.pastas (id) -- Chave estrangeira para pastas
 )
-    collate = utf8mb4_general_ci;
+    collate = utf8mb4_general_ci;                    -- Collation para suportar caracteres especiais
 
+-- Criação do índice para a coluna 'id_pasta' na tabela 'pasta_user'
 create or replace index id_pasta
     on artorganizer.pasta_user (id_pasta);
 
+-- Criação do índice para a coluna 'id_user' na tabela 'pasta_user'
 create or replace index id_user
     on artorganizer.pasta_user (id_user);
 
+-- Criação da tabela 'rec_senha'
 create or replace table artorganizer.rec_senha
 (
-    id             int auto_increment
-        primary key,
-    token          varchar(8) not null,
-    id_user        int        not null,
-    data_expiracao datetime   not null,
+    id int auto_increment primary key,            -- Identificador único do registro de senha
+    token varchar(8) not null,                    -- Token de recuperação de senha
+    id_user int not null,                         -- Referência ao usuário
+    data_expiracao datetime not null,             -- Data de expiração do token
     constraint rec_senha_ibfk_1
-        foreign key (id_user) references artorganizer.usuarios (ID)
+        foreign key (id_user) references artorganizer.usuarios (ID) -- Chave estrangeira para usuários
 )
-    collate = utf8mb4_general_ci;
+    collate = utf8mb4_general_ci;                -- Collation para suportar caracteres especiais
 
+-- Criação do índice para a coluna 'id_user' na tabela 'rec_senha'
 create or replace index id_user
     on artorganizer.rec_senha (id_user);
-
